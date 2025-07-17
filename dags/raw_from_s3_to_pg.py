dd3b8818d4b63b623196ad74223bd35e8f536933
pumpum421 <yegor37rus@mail.ru>
2025-07-17 14:11:16 +0300

    Изменён pet_project_earthquake: - Добавлен новый dag_изменен_2

--- dags/raw_from_s3_to_pg.py
+++ dags/raw_from_s3_to_pg.py
@@ -72,7 +72,7 @@ def get_and_transfer_raw_data_to_ods_pg(**context):
             PORT 5432,
             DATABASE postgres,
             USER 'postgres',
-            PASSWORD 'postgres'
+            PASSWORD '{PASSWORD}'
         );
 
         ATTACH '' AS dwh_postgres_db (TYPE postgres, SECRET dwh_postgres);
